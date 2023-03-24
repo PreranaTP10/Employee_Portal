1f4ad5b8d526e9247117920f8f2b816014b7bdd6
Prerana Tanaji Pawar <pawarprerana9702@gmail.com>
2023-03-24 20:18:57 +0530

    Initial commit

---
+++ employee_app/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index, name='index'),
+    path('all_emp',views.show_emp, name='show_emp'),
+    path('add_emp',views.add_emp, name='add_emp'),
+    path('del_emp',views.del_emp, name='del_emp'),
+    path('del_emp/<int:emp_id>',views.del_emp, name='del_emp'),
+    path('filter_emp',views.filter_emp, name='filter_emp'),
+
+
+]
